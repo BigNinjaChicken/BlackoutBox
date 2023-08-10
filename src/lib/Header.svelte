05fb2981c344cc9c644d347f4219abdf99565e40
BigNinjaChicken <50557956+BigNinjaChicken@users.noreply.github.com>
2023-08-10 15:33:25 -0400

    Initial commit

---
+++ src/lib/Header.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+    import { invoke } from "@tauri-apps/api/tauri";
+
+    let name: string = "";
+    let greetMsg: string = "";
+
+    async function greet() {
+        // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+        greetMsg = await invoke("greet", { name });
+    }
+</script>
+
+<section class="">
+    <div class="is-widescreen">
+        <div class="columns">
+            <aside class="menu">
+                <!-- <p class="menu-label">General</p>
+                <ul class="menu-list">
+                    <li><a>Dashboard</a></li>
+                    <li><a>Customers</a></li>
+                </ul>
+                <p class="menu-label">Administration</p>
+                <ul class="menu-list">
+                    <li><a>Team Settings</a></li>
+                    <li>
+                        <a class="is-active">Manage Team</a>
+                        <ul>
+                            <li><a>Members</a></li>
+                        </ul>
+                    </li>
+                </ul> -->
+                <p class="menu-label mb-3">Black Out Box</p>
+                <ul class="menu-list mb-3">
+                    <li><a class="is-active" href="./">Dashboard</a></li>
+                </ul>
+                <ul class="menu-list mb-3">
+                    <ul class="menu-list">
+                        <li>
+                            <p class="menu-label">My Game Boxes</p>
+                            <ul>
+                                <li><a href="./">Box 1</a></li>
+                                <li><a href="./">Box 2</a></li>
+                                <li><a href="./">Box 3</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </ul>
+            </aside>
+            <div class="column content" />
+        </div>
+    </div>
+</section>
+
+<style>
+    .menu {
+        position: sticky;
+        display: inline-block;
+        vertical-align: top;
+        max-height: 100vh;
+        overflow-y: auto;
+        width: 200px;
+        top: 0;
+        bottom: 0;
+        padding: 30px;
+    }
+
+    .content {
+        display: inline-block;
+    }
+</style>
